9157b6d63b86f2dd0fe405d55b19ae99ab6617e1
Alex <alexbachurin91@gmail.com>
2021-09-16 09:22:01 +0300

    main section of page

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Main = () => {
     return (
-        <div>
-
-        </div>
+        <section className='main'>
+            <div className="main-center">
+                <div className="main-info">
+                    <h1>
+                        Payments infrastructure
+                        for the internet
+                    </h1>
+                    <p>Millions of companies of all sizes—from startups to Fortune 500s—use Stripe’s software and APIs to accept payments, send payouts, and manage their businesses online.</p>
+                    <button className="btn">Start now</button>
+                </div>
+                <div className="main-images">
+                    <img src="https://res.cloudinary.com/dljezd6qv/image/upload/v1631773064/react-submenu/phone.58d7e3d6_udu3b7.svg" alt="phone" className="phone-img" />
+                </div>
+            </div>
+        </section >
     )
 }
 
